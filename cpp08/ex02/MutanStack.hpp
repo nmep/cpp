78619ee4421f2d4f7dd6880e1570aeb78395f186
lgarfi <lyess15@sfr.fr>
2024-06-10 14:21:08 +0200

    qwer

---
+++ cpp08/ex02/MutanStack.hpp
@@ -0,0 +1,29 @@
+# ifndef MUTANTSTACK_HPP
+	# define MUTANTSTACK_HPP
+	# include <iostream>
+	# include <stack>
+	# include <iterator>
+
+	template <typename T, typename Container = std::deque<T> >
+
+	class MutantStack : public std::stack<T>
+	{
+		public:
+			MutantStack() : std::stack<T, Container> () {};
+			~MutantStack() {};
+			MutantStack( const MutantStack & copy ) {
+				*this = copy;
+			}
+			MutantStack&	operator=(MutantStack const & rhs) {
+				this->c = rhs->c;
+				return *this;
+			}
+			typedef typename MutantStack::container_type::iterator iterator;
+			iterator begin() {
+				return this->c.begin();
+			}
+			iterator end() {
+				return this->c.end();
+			}
+	};
+# endif
